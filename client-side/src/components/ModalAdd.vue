1c5bb87d7cf8d50e702de01ca259113cb5521ca6
Draconov05 <luis_medeiros96@hotmail.com>
2024-09-07 23:30:09 -0300

    feat: modais para adicionar e editar

---
+++ client-side/src/components/ModalAdd.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div class="h-[10vh] w-full border-b px-5 flex items-center justify-between">
+
+    </div>
+</template>  
+
+<script >
+export default {
+  props: [],
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client-side/src/components/ModalEdit.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div class="h-[10vh] w-full border-b px-5 flex items-center justify-between">
+
+    </div>
+</template>  
+
+<script >
+export default {
+  props: ["task_id"],
+  data() {
+    return {
+    }
+  },
+  mounted: {
+    getTask(){
+        
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
