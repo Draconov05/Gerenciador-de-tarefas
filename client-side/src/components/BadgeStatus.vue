abdbaedee38fc84a3684deecd626185df4b5348c
Draconov05 <luis_medeiros96@hotmail.com>
2024-09-07 23:29:21 -0300

    feat: card da task individual e badge para status e palavras chave

---
+++ client-side/src/components/BadgeStatus.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <div :class="badgeClass">
+      {{ value ?? "." }}
+    </div>
+</template>  
+
+<script >
+export default {
+  props: ["tipo","value"],
+  data() {
+    return {
+    }
+  },
+  computed: {
+    badgeClass(){
+      switch (this.tipo) {
+
+        case "pendente":
+          return "inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10";
+        case "Em Progresso":
+          return "inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10";
+        case "Concluído":
+          return "inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20";
+
+        case "key":
+          return "inline-flex items-center rounded-lg bg-white px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20";
+      
+        default:
+          break;
+      }
+      
+      return "inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10";
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client-side/src/components/TaskCard.vue
@@ -0,0 +1,61 @@
+
+<template>
+    <div class="border rounded-md h-[15vh] w-[20vw] px-2" @mouseover="show_menu = true" @mouseleave="show_menu = false" >
+        <div class="h-1/3 flex items-center justify-between">
+            <div>
+                {{ task.titulo }}
+            </div>
+            <div class="flex space-x-2 relative">
+                <BadgeStatus :value="task.status" :tipo="task.status"></BadgeStatus>
+                <div v-if="show_menu" class="flex absolute z-10 right-1 bg-white p-2 border rounded-md space-x-2">
+                    <div class="cursor-pointer" @click="OpenModalEdit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M4 21a1 1 0 0 0 .24 0l4-1a1 1 0 0 0 .47-.26L21 7.41a2 2 0 0 0 0-2.82L19.42 3a2 2 0 0 0-2.83 0L4.3 15.29a1.06 1.06 0 0 0-.27.47l-1 4A1 1 0 0 0 3.76 21 1 1 0 0 0 4 21zM18 4.41 19.59 6 18 7.59 16.42 6zM5.91 16.51 15 7.41 16.59 9l-9.1 9.1-2.11.52z"></path></svg>
+                    </div>
+                    <div class="cursor-pointer" @click="OpenDelete">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="h-1/3 overflow-hidden flex">
+            <a class="text-blue-300 underline " :href="task.link ? task.link[0].link : task.link">link</a>
+        </div>
+        <div class="h-1/3 space-x-2">
+            <span>Tags:</span>
+            <BadgeStatus v-for="(keyWord, index) in palavrasChave" :key="index" :value="keyWord" tipo="key"></BadgeStatus>
+        </div>
+        
+    </div>
+</template>  
+
+<script >
+
+import BadgeStatus from './BadgeStatus.vue'; 
+
+export default {
+    components: {BadgeStatus},
+    props: ["task"],
+    emits: ["modal_edit"],
+    data() {
+        return {
+            palavrasChave: [],
+            show_menu: false
+        }
+    },
+    mounted(){
+        this.palavrasChave = this.task.palavrasChaves.split(",")
+    },
+    methods:{
+        OpenModalEdit(){
+            this.$emit("modal_edit",this.task._id)
+        },
+        OpenDelete(){
+            this.$emit("delete_confirm",this.task._id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
