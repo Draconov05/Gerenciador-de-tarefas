de2e0b42797e3b26e1d01521d6faf57fd5c90f54
Draconov05 <luis_medeiros96@hotmail.com>
2024-09-08 15:27:25 -0300

    feat: modulo axios e service para captar noticias

--- Server-side/src/assuntos/assuntos.module.ts
+++ Server-side/src/assuntos/assuntos.module.ts
@@ -4,9 +4,10 @@ import { AssuntosController } from './assuntos.controller';
 import { AssuntosService } from './assuntos.service';
 import { LinksModule } from '../links/links.module';
 import { Assunto, AssuntoSchema } from './schemas/assunto.schema';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Assunto.name, schema: AssuntoSchema }]),LinksModule],
+  imports: [MongooseModule.forFeature([{ name: Assunto.name, schema: AssuntoSchema }]),LinksModule,HttpModule],
   controllers: [AssuntosController],
   providers: [AssuntosService]
 })
--- Server-side/src/links/links.service.ts
+++ Server-side/src/links/links.service.ts
@@ -9,11 +9,15 @@ export class LinksService {
     constructor(@InjectModel(Link.name) private linkModel: Model<Link>) {}
 
     async find(assunto): Promise<Link> { 
-        return await this.linkModel.findOne({AssuntoId: assunto}).exec();;
+        return await this.linkModel.findOne({AssuntoId: assunto}).exec();
       }
 
     async create(CreateLink: any): Promise<Link> {
         const createdLink = new this.linkModel(CreateLink);
         return createdLink.save();
     }
+
+    async findByLink(link: string): Promise<Link> {
+        return await this.linkModel.findOne({link: link}).exec();
+    } 
 }
