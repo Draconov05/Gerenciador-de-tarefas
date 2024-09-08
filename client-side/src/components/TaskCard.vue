c7fd2725cfbeaf5487af1ce5aee00d4410b4d87f
Draconov05 <luis_medeiros96@hotmail.com>
2024-09-08 15:47:32 -0300

    feat: delete e confirmacao de remoção front

--- client-side/src/components/TaskCard.vue
+++ client-side/src/components/TaskCard.vue
@@ -9,10 +9,13 @@
                 <BadgeStatus v-if="!show_menu" :value="localTask.status" :tipo="localTask.status"></BadgeStatus>
                 <div v-else class="flex space-x-2">
                     <div class="cursor-pointer" @click="OpenModalEdit" v-tooltip="'Editar'">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M4 21a1 1 0 0 0 .24 0l4-1a1 1 0 0 0 .47-.26L21 7.41a2 2 0 0 0 0-2.82L19.42 3a2 2 0 0 0-2.83 0L4.3 15.29a1.06 1.06 0 0 0-.27.47l-1 4A1 1 0 0 0 3.76 21 1 1 0 0 0 4 21zM18 4.41 19.59 6 18 7.59 16.42 6zM5.91 16.51 15 7.41 16.59 9l-9.1 9.1-2.11.52z"></path></svg>
+                        <i class='bx bx-pencil bx-sm' ></i>                    
+                    </div>
+                    <div class="cursor-pointer" @click="ShowNews" v-tooltip="'Noticias'">
+                        <i class='bx bx-news bx-sm' ></i>
                     </div>
                     <div class="cursor-pointer" @click="OpenDelete">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>
+                        <i class='bx bx-trash bx-sm' ></i>                    
                     </div>
                 </div>
                 
@@ -33,7 +36,7 @@ import BadgeStatus from './BadgeStatus.vue';
 export default {
     components: {BadgeStatus},
     props: ["task"],
-    emits: ["modal_edit"],
+    emits: ["modal_edit","delete_confirm"],
     data() {
         return {
             palavrasChave: [],
@@ -54,8 +57,11 @@ export default {
         OpenModalEdit(){
             this.$emit("modal_edit",this.task)
         },
+        ShowNews(){
+            
+        },
         OpenDelete(){
-            this.$emit("delete_confirm",this.task._id)
+            this.$emit("delete_confirm",this.task)
         }
     }
 }
--- client-side/src/pages/Tasks/index.vue
+++ client-side/src/pages/Tasks/index.vue
@@ -4,7 +4,7 @@
       <HeaderBar/>
       <!-- listgrid -->
        <div id="list" class="grid grid-cols-4 gap-4 p-5">
-          <TaskCard v-for="(task, index) in LocalTasks" :key="index" :task="task" @modal_edit="OpenModalEdit($event)"/>
+          <TaskCard v-for="(task, index) in LocalTasks" :key="index" :task="task" @modal_edit="OpenModalEdit($event)" @delete_confirm="OpenDeleteConfirm($event)"/>
           <CreateCard @modal_add="showModelAdd = true"/>
        </div>
     </div>
@@ -12,6 +12,16 @@
     <ModalGray :open="openModal" @close="closeModals()">
       <ModalEdit v-if="showModelEdit" :task="selectedTaskToEdit" @changed="GetTasks" @close="closeModals()"/>
       <ModalAdd v-else-if="showModelAdd" @changed="GetTasks" @close="closeModals()"/>
+
+      <div v-else-if="showDeleteConfirm" class="h-[20vh] bg-red-50 rounded-lg flex flex-col justify-center items-start p-5 space-y-2">
+        <span class="text-red-600 font-md">
+          Você tem certeza que dejesa deletar a tarefa "{{ selectedTaskToEdit.titulo }}" ?
+        </span>
+        <div class="space-x-2">
+          <button class="text-sm rounded-lg text-red-400 bg-red-100 px-2 py-1" @click="deleteTask">Deletar</button>
+          <button class="text-sm rounded-lg text-gray-400 bg-gray-100 px-2 py-1" @click="closeModals">Cancelar</button>
+        </div>
+      </div>
     </ModalGray>
 
 </template>  
@@ -43,12 +53,13 @@ export default {
       LocalTasks: [],
       showModelEdit: false,
       showModelAdd: false,
+      showDeleteConfirm: false,
       selectedTaskToEdit: ""
     }
   },
   computed: {
     openModal(){
-      return this.showModelEdit || this.showModelAdd;
+      return this.showModelEdit || this.showModelAdd || this.showDeleteConfirm;
     }
   },
   mounted() {
@@ -56,19 +67,30 @@ export default {
   },
   methods: {
     async GetTasks() {
-      console.log("Tasks Loading")
       axios.get("http://localhost:3000/assuntos").then( res => {
         this.LocalTasks = res.data;
+      })
+    },
+    async deleteTask(){
+      axios.delete("http://localhost:3000/assuntos/"+this.selectedTaskToEdit._id).then( () => {
+        this.GetTasks();
+        this.closeModals();
       })
     },
     closeModals(){
       this.showModelAdd = false;
       this.showModelEdit = false;
+      this.showDeleteConfirm = false;
     },
     OpenModalEdit(event){
       this.selectedTaskToEdit = event
       this.showModelEdit = true
-    }
+    },
+    OpenDeleteConfirm(event){
+      this.selectedTaskToEdit = event
+      this.showDeleteConfirm = true
+    },
+    
   }
 }
 </script>
