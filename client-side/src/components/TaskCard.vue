098366bd4005a9161da5ca3dfc7ec1eff8132e23
Draconov05 <luis_medeiros96@hotmail.com>
2024-09-08 15:25:54 -0300

    fix: alterado variavel do card para que seja atualizado

--- client-side/src/components/TaskCard.vue
+++ client-side/src/components/TaskCard.vue
@@ -3,10 +3,10 @@
     <div class="border rounded-md h-[15vh] w-[20vw] px-2" @mouseover="show_menu = true" @mouseleave="show_menu = false" >
         <div class="h-1/3 flex items-center justify-between">
             <div>
-                {{ task.titulo }}
+                {{ localTask.titulo }}
             </div>
             <div class="flex space-x-2 relative">
-                <BadgeStatus v-if="!show_menu" :value="task.status" :tipo="task.status"></BadgeStatus>
+                <BadgeStatus v-if="!show_menu" :value="localTask.status" :tipo="localTask.status"></BadgeStatus>
                 <div v-else class="flex space-x-2">
                     <div class="cursor-pointer" @click="OpenModalEdit" v-tooltip="'Editar'">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M4 21a1 1 0 0 0 .24 0l4-1a1 1 0 0 0 .47-.26L21 7.41a2 2 0 0 0 0-2.82L19.42 3a2 2 0 0 0-2.83 0L4.3 15.29a1.06 1.06 0 0 0-.27.47l-1 4A1 1 0 0 0 3.76 21 1 1 0 0 0 4 21zM18 4.41 19.59 6 18 7.59 16.42 6zM5.91 16.51 15 7.41 16.59 9l-9.1 9.1-2.11.52z"></path></svg>
@@ -17,10 +17,6 @@
                 </div>
                 
             </div>
-            
-        </div>
-        <div class="h-1/3 overflow-hidden flex">
-            <a class="text-blue-300 underline " :href="task.link ? task.link[0].link : task.link">link</a>
         </div>
         <div class="h-1/3 space-x-2">
             <span>Tags:</span>
@@ -41,7 +37,14 @@ export default {
     data() {
         return {
             palavrasChave: [],
-            show_menu: false
+            show_menu: false,
+            localTask: this.task
+        }
+    },
+    watch: {
+        task(value){
+            this.localTask = value;
+            this.palavrasChave = value.palavrasChaves.split(",")
         }
     },
     mounted(){
