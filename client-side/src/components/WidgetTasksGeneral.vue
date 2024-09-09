3a74907af2a6faeb8be44fd2949c39a5adfa3dcc
Draconov05 <luis_medeiros96@hotmail.com>
2024-09-09 01:05:44 -0300

    feat: Widgets de dashboard

---
+++ client-side/src/components/WidgetTasksGeneral.vue
@@ -0,0 +1,62 @@
+
+<template>
+    <div>
+        <span>
+            Quantidade de tarefas:
+        </span>
+        <div class="flex space-x-1">
+            <div class="w-[45%] h-[10vh] border bg-gray-50 flex justify-center items-center rounded-lg">
+                <span class="text-gray-400">
+                    Pendentes: {{ LocalTasksPendente.length }}
+                </span>
+            </div>
+            <div class="w-[45%] h-[10vh] border bg-blue-50 flex justify-center items-center rounded-lg">
+                <span class="text-blue-400">
+                    Em Progresso: {{ LocalTasksEmProgresso.length }}
+                </span>
+            </div>
+            <div class="w-[45%] h-[10vh] border bg-green-50 flex justify-center items-center rounded-lg">
+                <span class="text-green-400">
+                    Concluidos: {{ LocalTasksConcluido.length }}
+                </span>
+            </div>
+        </div>
+    </div>
+</template>  
+
+<script >
+
+export default {
+  components: {
+  },
+  props: ["tasks"],
+  data() {
+    return {
+      LocalTasksPendente: [],
+      LocalTasksEmProgresso: [],
+      LocalTasksConcluido: [],
+    }
+  },
+  computed: {
+  },
+  mounted() {
+    this.filterTasks()
+  },
+  methods: {
+    filterTasks(){
+        this.LocalTasksPendente = this.tasks.filter(res => {
+            return res.status == "Pendente"
+        });
+        this.LocalTasksEmProgresso = this.tasks.filter(res => {
+            return res.status == "Em Progresso"
+        });
+        this.LocalTasksConcluido = this.tasks.filter(res => {
+            return res.status == "Concluído"
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client-side/src/components/WidgetTasksToday.vue
@@ -0,0 +1,68 @@
+
+<template>
+    <div>
+        <span>
+            Tarefas criadas hoje ({{ $moment(new Date()).locale('pt-br').format('L') }}):
+        </span>
+        <div class="flex space-x-1">
+            <div class="w-[45%] h-[10vh] border bg-gray-50 flex justify-center items-center rounded-lg">
+                <span class="text-gray-400">
+                    Pendentes: {{ LocalTasksPendente.length }}
+                </span>
+            </div>
+            <div class="w-[45%] h-[10vh] border bg-blue-50 flex justify-center items-center rounded-lg">
+                <span class="text-blue-400">
+                    Em Progresso: {{ LocalTasksEmProgresso.length }}
+                </span>
+            </div>
+            <div class="w-[45%] h-[10vh] border bg-green-50 flex justify-center items-center rounded-lg">
+                <span class="text-green-400">
+                    Concluidos: {{ LocalTasksConcluido.length }}
+                </span>
+            </div>
+        </div>
+    </div>
+</template>  
+
+<script >
+
+export default {
+  components: {
+  },
+  props: ["tasks"],
+  data() {
+    return {
+      LocalTasksPendente: [],
+      LocalTasksEmProgresso: [],
+      LocalTasksConcluido: [],
+    }
+  },
+  computed: {
+  },
+  mounted() {
+    this.filterTasks()
+  },
+  methods: {
+    filterTasks(){
+        var today = this.$moment(new Date()).locale('pt-br').format('L');
+
+        var totalToday = this.tasks.filter(res => {
+            return this.$moment(res.dataCriacao).locale('pt-br').format('L') == today
+        });
+
+        this.LocalTasksPendente = totalToday.filter(res => {
+            return res.status == "Pendente"
+        });
+        this.LocalTasksEmProgresso = totalToday.filter(res => {
+            return res.status == "Em Progresso"
+        });
+        this.LocalTasksConcluido = totalToday.filter(res => {
+            return res.status == "Concluído"
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
