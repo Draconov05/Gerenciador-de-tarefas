447212e2fe9750f5bf3b7dec495a802cf6b08321
Draconov05 <luis_medeiros96@hotmail.com>
2024-09-09 02:33:12 -0300

    fix: correção no router

--- client-side/src/App.vue
+++ client-side/src/App.vue
@@ -1,9 +1,21 @@
 <template>
+  <div class="flex items-center space-x-4 py-1" @click="home">
+      <div class="flex items-center space-x-1 border rounded-full bg-gray-50 pr-2">
+          <img class="h-14 w-14" src="https://i.ibb.co/T1fnF5X/logo.png" alt="" srcset="">
+          <span class="font-sm">TaskGen</span>
+      </div>
+      <RouterLink to="/">Dashboard</RouterLink>
+      <RouterLink to="/tarefas">Tarefas</RouterLink>
+  </div>
   <router-view/>
 </template>
 
 <script>
+
 export default {
+  components: {
+    
+  },
   name: 'App',
 }
 </script>
--- client-side/src/main.js
+++ client-side/src/main.js
@@ -1,9 +1,34 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VTooltip from 'v-tooltip'
+import moment from 'moment'
 
-const app = createApp(App)
+// Load Locales ('en' comes loaded by default)
+require('moment/locale/pt-br');
 
-app.use(VTooltip)
+moment.locale('pt-br');
 
+// Components
+import IndexTasks from './pages/Tasks/index.vue'
+import TaskNews from './pages/Tasks/noticias.vue'
+import DashboardIndex from './pages/Dashboard/index.vue'
+
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+const routes = [
+    { path: '/', name: 'home', redirect: '/dashboard' },
+    { path: '/dashboard', component: DashboardIndex, name: 'dashboard' },
+    { path: '/tarefas', component: IndexTasks, name: 'listagem' },
+    { path: '/noticias', component: TaskNews, name: 'noticias' }
+]
+
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+});
+
+const app = createApp(App).use(router)
+
+app.config.globalProperties.$moment = moment
+
+app.use(moment);
 app.mount('#app')
